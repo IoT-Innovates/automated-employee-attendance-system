9f3d2630ec9e023c0e6a052ffc982a48095e51a8
kavin <kavin@DARK-GHOST>
2025-12-17 22:32:28 +0530

    Add project files.

---
+++ Automated Employee Attendance System/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Automated_Employee_Attendance_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
