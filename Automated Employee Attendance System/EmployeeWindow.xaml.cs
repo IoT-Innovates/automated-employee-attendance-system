20f4e9d286e684a18880ee18452870b598af54a0
Design And Planing <Design And Planing@WIN-1J8RFCELTOA>
2025-12-24 13:19:46 +0530

    set fingerprint animation and navigations

--- Automated Employee Attendance System/EmployeeWindow.xaml.cs
+++ Automated Employee Attendance System/EmployeeWindow.xaml.cs
@@ -8,21 +8,45 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XamlAnimatedGif;
 
 namespace Automated_Employee_Attendance_System
 {
     /// <summary>
     /// Interaction logic for EmployeeWindow.xaml
     /// </summary>
-    public partial class EmployeeWindow : Page
+    public partial class EmployeeWindow : UserControl
     {
         public EmployeeWindow()
         {
             InitializeComponent();
+            Loaded += LoadingWindow_Loaded; // window render වෙන විට
+        }
+
+        private async void LoadingWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // GIF load background thread
+            await Task.Run(() =>
+            {
+                string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+                string gifPath = System.IO.Path.Combine(baseFolder, "UI", "Fingerprint_biometric_scan.gif");
+                var gifUri = new Uri(gifPath, UriKind.Absolute);
+
+                Dispatcher.Invoke(() =>
+                {
+                    AnimationBehavior.SetSourceUri(MyGifImage, gifUri);
+                    AnimationBehavior.SetRepeatBehavior(MyGifImage, System.Windows.Media.Animation.RepeatBehavior.Forever);
+                });
+            });
+
+          
+
+         
         }
     }
 }
--- Automated Employee Attendance System/MainWindow.xaml.cs
+++ Automated Employee Attendance System/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Drawing;
+using System.IO;
+
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+
+using System.Windows.Data;
+using System.Windows.Documents;
+
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -19,6 +25,45 @@ namespace Automated_Employee_Attendance_System
         public MainWindow()
         {
             InitializeComponent();
+           
         }
+
+
+
+        #region Navigation
+
+
+        private void EmployeeWindow_Click(object sender, RoutedEventArgs e) => LoadView(new EmployeeWindow());
+
+
+        private void LoadView(UserControl view)
+        {
+            TranslateTransform trans = new TranslateTransform();
+            view.RenderTransform = trans;
+            view.Opacity = 0;
+
+            DoubleAnimation slideAnim = new DoubleAnimation
+            {
+                From = 50,
+                To = 0,
+                Duration = TimeSpan.FromMilliseconds(400),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            DoubleAnimation fadeAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromMilliseconds(400)
+            };
+
+            MainContent.Content = view;
+
+            trans.BeginAnimation(TranslateTransform.YProperty, slideAnim);
+            view.BeginAnimation(UserControl.OpacityProperty, fadeAnim);
+        }
+
+        #endregion
+
     }
 }
